4f9a278e6ebdce14899b50945e09a46f502bbbb9
Juan <juanabascalsanchez@gmail.com>
2018-05-03 20:50:43 +0200

    Evaluation bug fixed #3

--- eval.py
+++ eval.py
@@ -19,13 +19,14 @@ from __future__ import print_function
 
 import tensorflow as tf
 import logging
-import input, model
+import input
+import model
 
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('ckpt_dir', 'data/checkpoints/',
                            """Directory where to restore a model""")
-tf.app.flags.DEFINE_string('eval_dir', 'data/trained/adam_0.005/',
+tf.app.flags.DEFINE_string('eval_dir', '/home/uc3m4/Documentos/Trained/ft_flowers/lr_ed_0.4',
                            """Directory where to restore the fine tuning model""")
 tf.app.flags.DEFINE_string('save_dir', 'data/train/flowers',
                            """Directory where to write event logs """
@@ -34,24 +35,28 @@ tf.app.flags.DEFINE_string('log_dir', 'data/train/log',
                            """Directory where to write event logs.""")
 tf.app.flags.DEFINE_integer('max_steps', 2000,
                             """Number of batches to run.""")
-tf.app.flags.DEFINE_integer('batch_size', 32,
+tf.app.flags.DEFINE_integer('batch_size', 1,
                             """Size of batches.""")
 
 
 def train():
 
-    with tf.Graph().as_default() as g:
-        global_step = tf.train.get_or_create_global_step()
-
-        #With is_training=False, we get the image without distortions
-        iterator = input.consume_tfrecord(is_training=False, batch_size=1)
+    with tf.Graph().as_default():
+        # With is_training=False, we get the image without distortions
+        iterator = input.consume_tfrecord(is_training=False, batch_size=FLAGS.batch_size)
         images_batch, labels_batch = iterator.get_next()
 
-        # Num_classes is None for fine tunning
+        # Num_classes is None for fine tuning. You need to have the proper scope.
+        # From the original model we only need the bottlenecks.
         with tf.contrib.slim.arg_scope(model.inception_v3_arg_scope()):
-            bottleneck, end_points = model.inception_v3(images_batch, num_classes=None, is_training=False)
+            bottleneck, end_points = model.inception_v3(images_batch, num_classes=None, is_training=False,
+                                                        dropout_keep_prob=1)
 
+        # We pass the bottleneck generated in the step before to the new classifier.
         logits = model.fine_tuning(bottleneck, end_points)
+
+        # Get the class with the highest score
+        predictions = tf.nn.top_k(logits, k=1)
 
         # For eval you need to restore the fine tuning model
         saver = tf.train.Saver(tf.global_variables('InceptionV3'))
@@ -59,34 +64,29 @@ def train():
 
         init = tf.global_variables_initializer()
 
+        logger = init_logger()
+        logger.info("Eval starts...")
+
         with tf.Session() as sess:
             sess.run(init)
 
             saver.restore(sess, tf.train.latest_checkpoint(FLAGS.ckpt_dir))
             saver_ft.restore(sess, tf.train.latest_checkpoint(FLAGS.eval_dir))
 
-            tf.summary.image(tensor=images_batch, name="Image")
+            success = 0
+            total = 0
+            exec_next_step = True
+            while exec_next_step is True:
+                try:
+                    total += 1
+                    predicted, image, label = sess.run([predictions, images_batch, labels_batch])
 
-            logger = init_logger()
-            logger.info("Eval starts...")
-
-            #Counter for the succes predictions
-            correct = 0
-
-            #The range is from 1 to number of image of the eval dataset.
-            for i in range(1, 1170):
-                images, labels = sess.run([images_batch, labels_batch])
-
-                #Get the class with the highest score
-                predicted_class = sess.run(tf.nn.top_k(logits, k=1)[1][0])
-
-                #If the predicted class is the correct one, we add one to the counter
-                if predicted_class == labels:
-                    correct += 1
-
-                logger.info('Success rate: %.2f of %i examples', correct/i*100, i)
-
-            logger.info("Eval ends...")
+                    if predicted.indices[0][0] == label[0]:
+                        success += 1
+                    logger.info('Success rate: %.2f of %i examples', success / total * 100, total)
+                except tf.errors.OutOfRangeError:
+                    logger.info("Eval ends...")
+                    exec_next_step = False
 
 
 def main(argv=None):
